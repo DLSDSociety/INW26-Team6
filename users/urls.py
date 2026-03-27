34b7c4d6e845e7eb92f3757478e301f682cfdec5
Jasminkhatun <jk5077826@gmail.com>
2026-03-27 20:26:41 +0530

    Initial clean commit

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RegisterView, LoginView, ProfileView, UpdateProfileView, AdminOnlyView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('profile/', ProfileView.as_view()),
+    path('profile/update/', UpdateProfileView.as_view()),
+    path('admin-only/', AdminOnlyView.as_view()),
+]
